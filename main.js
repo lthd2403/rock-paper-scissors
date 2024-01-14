488d4ac17a3724f9884925b0279e72dde357a114
Kevin Le <letuanhaidang369@gmail.com>
2024-01-14 17:22:03 -0600

    Complete functions to check winner of each round and print message

--- main.js
+++ main.js
@@ -1,30 +1,49 @@
-const playerText = document.querySelector("#playerText");
-const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
-const choiceButton = document.querySelectorAll(".buttonClass");
+document.addEventListener("DOMContentLoaded", function() {
+    const playerText = document.querySelector("#playerText");
+    const computerText = document.querySelector("#computerText");
+    const resultText = document.querySelector("#resultText");
+    const choices = document.querySelectorAll(".buttonclass");
 
-let player;
-let computer;
-let result;
+    let player;
+    let computer;
 
-choiceButton.forEach(button => button.addEventListener("click", () => {
-    player = playerText.textContent;
-    computerChoice();
-    
-}))
+    choices.forEach(button => button.addEventListener("click", () => {
+        player = button.textContent;
+        computerChoice();
+        playerText.textContent = `Player: ${player}`;
+        computerText.textContent = `Computer: ${computer}`;
+        resultText.textContent = checkWinner();
+    }));
 
-function ComputerChoice(){
-    const randNum = Math.floor(Math.random() * 3);
+    function computerChoice() {
+        const randNum = Math.floor(Math.random() * 3);
 
-    switch(randNum) {
-        case 0:
-            computer = "Rock";
-            break;
-        case 1:
-            computer = "Paper";
-            break;
-        case 2:
-            computer = "Scissors";
-            break;
+        switch(randNum) {
+            case 0:
+                computer = "Rock";
+                break;
+            case 1:
+                computer = "Paper";
+                break;
+            case 2:
+                computer = "Scissors";
+                break;
+        }
     }
-}
+
+    function checkWinner() {
+        if (player === computer) {
+            com = computer.toLowerCase();
+            return "Draw! " + player + " ties " + com;
+        } 
+        else if (computer === "Rock") {
+            return (player === "Paper") ? "You win! Paper beats rock." : "You lose! Rock beats scissors.";
+        } 
+        else if (computer === "Paper") {
+            return (player === "Scissors") ? "You win! Scissors beats paper." : "You lose! Paper beats rock.";
+        } 
+        else if (computer === "Scissors") {
+            return (player === "Rock") ? "You win! Rock beats scissors." : "You lose! Scissors beats paper.";
+        }
+    }
+});
